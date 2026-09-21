20897474c799679c6a66cdde34d500879d1d62a2
gabi emi <gabiemi2021@gmail.com>
2026-09-21 11:03:57 -0300

    feat: integrate CRUD data into Card component and implement series fetching in ReadPage

---
+++ src/app/api/series/route.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { headers } from 'next/headers';
+import { NextResponse } from 'next/server';
+
+export async function GET(req) {
+    const { searchParams } = new URL (req.url);
+
+    try {
+        const resp = await axios.get(process.env.API_URL_SERIES, {
+            params: Object.fromEntries(searchParams),
+            headers: {'x-api-key': process.env.API_KEY}
+        });
+        return NextResponse.json(resp.data);
+    } catch (error) {
+        const status = error.response?.status || 500;
+        const data = error.response?.data || {error: 'Erro ao buscar as séries.'};
+
+        return NextResponse.json(data, {status});
+    }
+}
